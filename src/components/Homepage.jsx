77c37af1b64e622bb4823eea115af62fa3e47b4d
biagioo <mendolia2328@gmail.com>
2021-10-04 11:41:51 -0400

    added columns to stats

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -21,21 +21,32 @@ const Homepage = () => {
       <Title level={2} className='heading'>
         Global Crypto Stats
       </Title>
-      <Row>
+      <Row gutter={[32, 32]}>
         <Col span={12}>
-          <Statistic title='Total Crytpocurrencies' value={globalStats.total} />
+          <Statistic title='Total Cryptocurrencies' value={globalStats.total} />
+        </Col>
+        <Col span={12}>
           <Statistic
             title='Total Exchanges'
             value={millify(globalStats.totalExchanges)}
           />
+        </Col>
+        <Col span={12}>
           <Statistic
-            title='Total Market Cap'
-            value={millify(globalStats.totalMarketCap)}
+            title='Total Market Cap:'
+            value={`$${millify(globalStats.totalMarketCap)}`}
           />
+        </Col>
+        <Col span={12}>
           <Statistic
             title='Total 24h Volume'
-            value={millify(globalStats.total24hVolume)}
+            value={`$${millify(globalStats.total24hVolume)}`}
           />
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total Cryptocurrencies' value={globalStats.total} />
+        </Col>
+        <Col span={12}>
           <Statistic
             title='Total Markets'
             value={millify(globalStats.totalMarkets)}
@@ -44,10 +55,10 @@ const Homepage = () => {
       </Row>
       <div className='home-heading-container'>
         <Title level={2} className='home-title'>
-          Top 10 Cryptocurrencies In The World
+          Top 10 Cryptos In The World
         </Title>
         <Title level={3} className='show-more'>
-          <Link to='/cryptocurrencies'>Show More</Link>
+          <Link to='/cryptocurrencies'>Show more</Link>
         </Title>
       </div>
       <Cryptocurrencies simplified />
@@ -55,8 +66,8 @@ const Homepage = () => {
         <Title level={2} className='home-title'>
           Latest Crypto News
         </Title>
-        <Title level={3} className='show-more'>
-          <Link to='/news'>Show More</Link>
+        <Title level={3}>
+          <Link to='/news'>Show more</Link>
         </Title>
       </div>
       <News simplified />
